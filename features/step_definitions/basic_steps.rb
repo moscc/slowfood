ab97f3fb8cb51fa78a47a447372da626a2d7dc13
nader <alghazu@gmail.com>
2018-04-25 17:08:31 +0200

    Fixed a basic step deffinition

--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,5 +2,5 @@ Given("I visit the application") do
   visit root_path
 end
 Given("there is a dish called {string}") do |string|
-  @products = Product.create(name:string)
+  @products = Product.create(name:string, desc: 'Whatever', price:100)
 end
