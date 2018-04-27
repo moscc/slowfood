21e7a054ec21a93943be3fe5866bcccab8b510c0
nader <alghazu@gmail.com>
2018-04-27 17:20:58 +0200

    Added edit function for products.WIP Saving after edit.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,12 +3,11 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
-  def new
-  end
+  # def new
+  # end
 
   def create
-    @product = Product.new(user_params)
-
+    @product = Product.new(product_params)
     if @product.save
       redirect_to @product
     else
@@ -16,13 +15,24 @@ class ProductsController < ApplicationController
     end
   end
 
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to @product
+    else
+      render 'edit'
+    end
+  end
+
   def show
     @product = Product.find(params[:id])
   end
 
+  def delete
+
   private
 
-  def user_params
+  def product_params
     params[:product][:price] = params[:product][:price].to_i
     params[:product][:category_id] = params[:product][:category_id].to_i
     params.require(:product).permit(:name, :desc, :price, :category_id )
